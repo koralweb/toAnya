2cfcd50657660f0e5547d458ff76e4295a482a77
koralweb <dev@koralweb.com>
2024-04-17 09:17:35 +0300

    Start

---
+++ src/components/MainComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function MainComponent() {
+        return (
+        <div>
+            <h1>Hello Anna!</h1>
+        </div>
+    );
+}
+
+const styles = {
+
+}
+
+export default MainComponent;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+let path = require("path");
+let MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let HTMLWebpackPlugin = require("html-webpack-plugin");
+let {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+let conf = {
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        publicPath: "/",
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "."),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: "/node_modules/",
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "main.css",
+        }),
+        new HTMLWebpackPlugin({
+            template: "./index.html",
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
+
+module.exports = (env, options) => {
+    let isProd = options.mode === "production";
+    conf.devtool = isProd ? false : "eval-cheap-module-source-map";
+    conf.output.filename = isProd ? "[contenthash].[name].js" : "main.js";
+    return conf;
+};
